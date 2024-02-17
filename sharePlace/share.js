9170ff08ba0c907f4d5c5d177a6da52a110ee50c
Madhav <madhav.shar06ma@gmail.com>
2024-02-17 22:46:22 +0530

    fuckit

--- sharePlace/share.js
+++ sharePlace/share.js
@@ -1,3 +1,18 @@
-function loadAddress(){
-    
+import { renderMapForCurrentLocation } from "../script.js/utility/map.js";
+
+function loadAddress(coordinates , address){
+    renderMapForCurrentLocation(coordinates);
+    const title = document.querySelector(".header h1");
+    title.textContent = address;
 }
+
+const url = new URL(location.href);
+const queryParameter = url.searchParams;
+const coords = {
+    lat:+queryParameter.get('lat'),
+    lon:+queryParameter.get('lng')
+};
+
+const address = queryParameter.get('address');
+
+loadAddress(coords , address);
